73667d907f8655762a3778f5261e0ee360bed6fd
Laksh Agrawal <122020648+LakshAgrawal28@users.noreply.github.com>
2026-08-18 11:10:50 +0530

    Add: construct-quad-tree - submission-1

---
+++ Data Structures & Algorithms/construct-quad-tree/submission-1.cpp
@@ -0,0 +1,67 @@
+/*
+// Definition for a QuadTree node.
+class Node {
+public:
+    bool val;
+    bool isLeaf;
+    Node* topLeft;
+    Node* topRight;
+    Node* bottomLeft;
+    Node* bottomRight;
+    
+    Node() {
+        val = false;
+        isLeaf = false;
+        topLeft = NULL;
+        topRight = NULL;
+        bottomLeft = NULL;
+        bottomRight = NULL;
+    }
+    
+    Node(bool _val, bool _isLeaf) {
+        val = _val;
+        isLeaf = _isLeaf;
+        topLeft = NULL;
+        topRight = NULL;
+        bottomLeft = NULL;
+        bottomRight = NULL;
+    }
+    
+    Node(bool _val, bool _isLeaf, Node* _topLeft, Node* _topRight, Node* _bottomLeft, Node* _bottomRight) {
+        val = _val;
+        isLeaf = _isLeaf;
+        topLeft = _topLeft;
+        topRight = _topRight;
+        bottomLeft = _bottomLeft;
+        bottomRight = _bottomRight;
+    }
+};
+*/
+
+class Solution {
+public:
+    int check(int i1, int j1, int i2, int j2, vector<vector<int>>& grid) {
+        int ones = 0, zeros = 0;
+        for(int i = i1; i <= i2; i++) {
+            for(int j = j1; j <= j2; j++) {
+                if(grid[i][j]) ones++;
+                else zeros++;
+            }
+        }
+        if(zeros && ones) return -1;
+        return zeros ? 0 : 1;
+    }
+    Node* f(int i1, int j1, int i2, int j2, vector<vector<int>>& grid) {
+        int val = check(i1, j1, i2, j2, grid);
+        if(val != -1) return new Node(val, true);
+        
+        return new Node(false, false, f(i1, j1, (i1+i2)/2, (j1+j2)/2, grid),
+         f(i1, (j1+j2+2)/2, (i1+i2)/2, j2, grid), f((i1+i2+2)/2, j1, i2, (j1+j2)/2, grid),
+         f((i1+i2+2)/2, (j1+j2+2)/2, i2, j2, grid));
+    }
+    Node* construct(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int i1 = 0, j1 = 0, i2 = n-1, j2 = n-1;
+        return f(0, 0, n-1, n-1, grid);
+    }
+};
